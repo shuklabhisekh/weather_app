7c29cc586f8cf06dd1eb2defe0f06c3cb2f4b904
Abhishek Shukla <shuklabhisekh@gmail.com>
2022-06-24 10:59:13 +0530

    added geolocation

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,38 @@
 import "../css/Navbar.css";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { useEffect } from "react";
 function Navbar() {
+  const takeLocation = () => {
+    if ("geolocation" in navigator) {
+      // check if geolocation is supported/enabled on current browser
+      navigator.geolocation.getCurrentPosition(
+        function success(position) {
+          // for when getting location is a success
+          console.log(
+            "latitude",
+            position.coords.latitude,
+            "longitude",
+            position.coords.longitude
+          );
+        },
+        function error(error_message) {
+          // for when getting location results in an error
+          console.error(
+            "An error has occured while retrieving   location",
+            error_message
+          );
+        }
+      );
+    } else {
+      // geolocation is not supported
+      // get your location some other way
+      console.log("geolocation is not enabled on this browser");
+    }
+  };
+  useEffect(() => {
+    takeLocation();
+  });
   return (
     <div>
       <div className="searchbar">
